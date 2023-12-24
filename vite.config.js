4db3f78d4bd8b0eb437b3d7876d569c64cec9f99
sebascs04 <sebastiancamsal2002@gmail.com>
2023-12-23 20:55:40 -0500

    Cuarto commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://sebascs04.github.io/ThePortfolio'
+  base: 'https://sebascs04.github.io/Miportafolio'
 })
